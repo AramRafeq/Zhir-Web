05c5fbdf5b18ab58b78ff89162040bbaf1063eba
aram rafeq <aramrafeq2@gmail.com>
2021-10-04 17:44:52 +0300

    shitty ui

--- components/layout/Index.jsx
+++ components/layout/Index.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
-import { Layout, Menu, Card } from 'antd';
+import {
+  Layout, Menu, Card, Row, Col, Button,
+} from 'antd';
 
-export default function Index(props) {
-  const { children } = props;
-  return (
-    <Layout>
-      <Layout.Sider>
+class Index extends React.Component {
+  constructor() {
+    super();
+    this.initialState = () => ({
+      windowHeight: 0,
 
-        <Menu theme="dark" mode="vertical" defaultSelectedKeys={['2']}>
-          <Menu.Item key="logo"><img src="/assets/white-logo.png" className="dashboard-logo" alt="logo" /></Menu.Item>
-          <Menu.Item key="home">ماڵەوە</Menu.Item>
-          <Menu.Item key="home">ماڵەوە</Menu.Item>
-          <Menu.Item key="home">ماڵەوە</Menu.Item>
-          <Menu.Item key="home">ماڵەوە</Menu.Item>
-          <Menu.Item key="home">ماڵەوە</Menu.Item>
-        </Menu>
-      </Layout.Sider>
-      <Layout.Content style={{ padding: '0 50px' }}>
-        <Card>
-          {children}
-        </Card>
-      </Layout.Content>
-      {/* <Layout.Footer style={{ textAlign: 'center' }}>گەشەپێدان و دروستکردن گەشەپێدەرانی کورد</Layout.Footer> */}
-    </Layout>
-  );
+    });
+    this.state = this.initialState();
+  }
+
+  componentDidMount() {
+    this.setState({ windowHeight: window.innerHeight });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { windowHeight } = this.state;
+    return (
+      <Layout style={{ height: windowHeight }}>
+        <Layout.Sider>
+          <div className="sidebar-logo-container">
+            <Row justify="center" align="middle">
+              <Col span={24} style={{ padding: 50 }}>
+                <img src="/assets/white-logo.png" className="logo" alt="logo" />
+              </Col>
+              <Col span={24}>
+                <p style={{ color: 'white', width: '100%', textAlign: 'center' }}>هەموو شتەکی فشە لێرە ببینە</p>
+              </Col>
+            </Row>
+          </div>
+          <Menu theme="dark" mode="vertical" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">ماڵەوە</Menu.Item>
+            <Menu.Item key="2">سەرتاشخانە</Menu.Item>
+            <Menu.Item key="my">میوەجات</Menu.Item>
+            <Menu.Item key="4">شێتخانە</Menu.Item>
+            <Menu.Item key="5">شەماعیە</Menu.Item>
+          </Menu>
+          {/* <Row>
+            <Col span={24}>
+              <Button size="large" block>سڵاوی جیهان</Button>
+            </Col>
+          </Row> */}
+        </Layout.Sider>
+        <Layout.Content style={{ padding: '0 50px' }}>
+          <Card style={{ height: '100%' }}>
+            {children}
+          </Card>
+        </Layout.Content>
+        {/* <Layout.Footer style={{ textAlign: 'center' }}>گەشەپێدان و دروستکردن گەشەپێدەرانی کورد</Layout.Footer> */}
+      </Layout>
+    );
+  }
 }
+export default Index;
